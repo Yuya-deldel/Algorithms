73b4445d0e5a7c1068da7546811f770a60028507
YD <deltagraviton@gmail.com>
2024-11-02 00:10:07 +0900

    Add files via upload

--- string_data_algorithm/Rolling_Hash_mod_Mersenne.py
+++ string_data_algorithm/Rolling_Hash_mod_Mersenne.py
@@ -39,33 +39,22 @@ def modMersenne(x):
 # define conversion function: Char => Int
 def Char_to_Int(s):
     return ord(s) - ord('a') + 1
-    
-# Hash calculation: return hash(s[l ~ r])
-def Hash(S, Base, l, r):          
-    N = len(S)
-    H = [0] * (N + 1)
-    B = [1] * (N + 1)
-    for i in range(1, N+1):
-        H[i] = multiplication(H[i-1], Base) + Char_to_Int(S[i-1])
-        B[i] = multiplication(B[i-1], Base)
-    
-    return modMersenne(H[r] - multiplication(H[l-1], B[r-l+1]))
 
-# Matching
-N, Q = [int(e) for e in input().split()]
-S = input()
-Base = random.randrange(27, 50)
-H = [0] * (N + 1)
-B = [1] * (N + 1)
-for i in range(1, N+1):
-    H[i] = multiplication(H[i-1], Base) + Char_to_Int(S[i-1])
-    B[i] = multiplication(B[i-1], Base)
+class RollingHash:
+    def __init__(self, String, Base):
+        N = len(String)
+        self.length = N
 
-for k in range(Q):
-    l1, r1, l2, r2 = [int(e) for e in input().split()]
-    h1 = modMersenne(H[r1]  - multiplication(H[l1 - 1], B[r1 - l1 + 1]))
-    h2 = modMersenne(H[r2]  - multiplication(H[l2 - 1], B[r2 - l2 + 1]))
-    if h1 == h2:
-        print("Yes")
-    else:
-        print("No")
+        H = [0] * (N + 1)
+        B = [1] * (N + 1)
+        for i in range(1, N+1):
+            H[i] = multiplication(H[i-1], Base) + Char_to_Int(String[i-1])
+            B[i] = multiplication(B[i-1], Base)
+
+        self.B = B
+        self.H = H
+
+    # hash(S[l ~ r])
+    def Hash_value(self, l, r):
+        return modMersenne(self.H[r] - multiplication(self.H[l - 1], self.B[r - l + 1]))
+    
